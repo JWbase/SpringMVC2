4fe059f51c89740a38bd51eac145441a9d33df60
JWbase <jungwoo1002@gmail.com>
2023-03-22 00:20:07 +0900

    - 스프링 오류페이지

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -4,10 +4,7 @@ import hello.itemservice.web.validation.form.ItemSaveForm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -26,4 +23,5 @@ public class ValidationItemApiController {
         log.info("성공 로직 실행");
         return form;
     }
+
 }
